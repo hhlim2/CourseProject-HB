6a5d986e57431922259c51f55767bdd230a61eb3
hhlim2 <60621141+hhlim2@users.noreply.github.com>
2023-11-17 15:55:20 -0800

    Add files via upload

---
+++ bm25.py
@@ -0,0 +1,74 @@
+import nltk
+import numpy as np
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+
+
+class BM25_algorithm:
+     
+    def __init__(self, corpus, k, b):
+        self.num_docs = len(corpus)
+        self.avgdl = sum(len(i) for i in corpus) / self.num_docs
+        self.k = k
+        self.b = b
+        self.corpus = corpus
+
+
+    #https://www.geeksforgeeks.org/removing-stop-words-nltk-python/#
+    def remove_stopwords(self):
+        corpus_updated = []
+        corpus = self.corpus
+
+        stop_words = stopwords.words('english')
+        for doc in corpus: 
+            '''doc = doc.lower().split(' ')
+            filtered_doc= []
+            for w in doc:
+                if w not in stop_words:
+                    filtered_doc.append(w)'''
+            filtered_doc = [w for w in doc.lower().split(' ') if w not in stop_words]
+
+            corpus_updated.append(filtered_doc)
+        return corpus_updated
+
+
+
+    
+    def calculate_scores(self, query):
+        query = query.split(' ')
+
+        cleaned_corpus = self.remove_stopwords()
+      
+        avg_dl = self.avgdl
+        k1 = self.k
+        b = self.b
+        score = []
+    
+        for doc in cleaned_corpus:
+            freq_qd = []
+            dl = len(doc)
+
+            for query_term in query:
+                freq_qd.append(doc.count(query_term))
+            
+ 
+            idf = [0 if i == 0 else 1/i for i in freq_qd]
+            
+            score_doc = 0
+            for i in range(len(freq_qd)):
+                score_doc += idf[i] * ((freq_qd[i] * (k1 + 1))/(freq_qd[i] + k1*(1-b + b*dl/avg_dl)))
+
+            score.append(score_doc)
+        
+        return score
+
+
+
+
+
+    
+
+
+         
+
--- news.py
+++ news.py
@@ -3,157 +3,151 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 import os
 import streamlit as st
-from langchain.document_loaders import WebBaseLoader
+from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from transformers import pipeline
 from rank_bm25 import BM25Okapi
 from transformers import T5Tokenizer, T5ForConditionalGeneration
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+from bm25 import BM25_algorithm
 
-
-from vectorstore import VectorStore
+import json
+# Python 3
+import http.client, urllib.parse
+#from vectorstore import VectorStore
 import numpy as np
+
+
+from huggingface_hub import login
+login(token='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')
+
+import warnings
+warnings.filterwarnings("ignore")
 
 #import environment variables
 load_dotenv()
-
-api_key = os.getenv('news_api', None)
-newscatcherapi = NewsCatcherApiClient(x_api_key=api_key) 
-
 
 st.title('CS410 News Retrieval')
 query = st.text_input('news question')
 
 
-def summarize(articles, input_query):
-    links = []
-    titles = []
-    for i in range(len(articles['articles'])):
-        links.append(articles['articles'][i]['link'])
-        titles.append(articles['articles'][i]['title'])
 
-    loader = WebBaseLoader(links)
-    loader.requests_kwargs = {'verify':False, 'timeout':15}
-    docs = loader.load()
 
-    CHUNK_SIZE = 3000
-    CHUNK_OVERLAP = 100
+# Load model directly
+#tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
+#model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
 
-    text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=CHUNK_SIZE,
-                chunk_overlap=CHUNK_OVERLAP
-            )
-    texts = text_splitter.split_documents(docs)
-
-    metadata = []
-    doc_content = []
-    for i in range(len(texts)):
-        metadata.append(texts[i].metadata)
-        doc_content.append(texts[i].page_content)
-    
-    tokenized_corpus = [doc.split(" ") for doc in doc_content]
-
-    bm25_corpus = BM25Okapi(tokenized_corpus)
-
-    vector_store = VectorStore()
-
-    vocabulary = set()
-    for sentence in doc_content:
-        tokens = sentence.lower().split()
-        vocabulary.update(tokens)
-
-    # Assign unique indices to words in the vocabulary
-    word_to_index = {word: i for i, word in enumerate(vocabulary)}
-
-    # Vectorization
-    sentence_vectors = {}
-    for sentence in doc_content:
-        tokens = sentence.lower().split()
-        vector = np.zeros(len(vocabulary))
-        for token in tokens:
-            vector[word_to_index[token]] += 1
-        sentence_vectors[sentence] = vector
-
-    # Storing in VectorStore
-    for sentence, vector in sentence_vectors.items():
-        vector_store.add_vector(sentence, vector)
-
-    # Searching for Similarity
-    query_sentence = input_query
-    query_vector = np.zeros(len(vocabulary))
-    query_tokens = query_sentence.lower().split()
-    for token in query_tokens:
-        if token in word_to_index:
-            query_vector[word_to_index[token]] += 1
-
-    bm25_similarity = vector_store.find_similar_vectors(query_vector, num_results=2)
-
-    #tokenized_query = input_query.split(" ")
-    #bm25_scores = bm25_corpus.get_scores(tokenized_query)
-
-    #relevant_passages = doc_content[np.argmax(bm25_similarity)]
-    #relevant_metadata = metadata[np.argmax(bm25_similarity)]
-    #similarity = np.max(bm25_similarity)
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large")
 
 
 
-    # Print similar sentences
-
-    relevant_passages = bm25_similarity[0][0]
-    
-    similarity = bm25_similarity[0][1]
-    #print(relevant_passages)
-
-    
-    # Load model directly
-
-    flan_tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
-    flan_model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
+tokenizer = tokenizer
+model= model
 
 
-   
-    falcon_tokenizer = AutoTokenizer.from_pretrained("tiiuae/falcon-40b")
-    falcon_model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-40b")
 
 
-    tokenizer = falcon_tokenizer
-    model= falcon_model
+with st.sidebar:
+    min_length_entry = st.number_input('minimum length of output', min_value=1, value = 50)
+    max_length_entry = st.number_input('maximum length of output', min_value= min_length_entry + 1, value = 150)
+    temperature_entry = st.slider('temperature', min_value=0.0, max_value=1.0, value=0.2, step=0.01)
+    top_p_entry = st.slider('top_p', min_value=0.0, max_value=1.0, value=0.15, step=0.01)
+    top_k_entry = st.number_input('top_k', min_value=0, max_value=100, value=50)
+    k1_entry = st.number_input('bm25 k1', min_value=0.0, max_value= 100.0, value=1.2)
+    b_entry = st.slider('bm25 b', min_value=0.0, max_value= 1.0, value=.75, step=0.01)
+    submit = st.button(label='submit')
 
-    input_text = f"using the following context: {relevant_passages}, answer the following question: {query}"
+if submit:
+    #model parameters
+    min_length = min_length_entry
+    max_length = max_length_entry
+    temperature = temperature_entry
+    top_p = k1_entry
+    top_k = top_k_entry
+
+    #bm25_parameters
+    k1 = k1_entry
+    b = b_entry
+else:
+    #default model parameters
+    min_length = 50
+    max_length = 150
+    temperature = 0.2
+    top_p = 0.15
+    top_k = 50
+
+    #default bm25_parameters
+    k1 = 1.2
+    b = 0.75
+
+loader = PyPDFLoader("CS441 Textbook.pdf")
+
+docs = loader.load()
+
+CHUNK_SIZE = 1500
+CHUNK_OVERLAP = 200
+
+text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=CHUNK_SIZE,
+            chunk_overlap=CHUNK_OVERLAP
+        )
+texts = text_splitter.split_documents(docs)
+
+metadata = []
+doc_content = []
+for i in range(len(texts)):
+    metadata.append(texts[i].metadata)
+    doc_content.append(texts[i].page_content)
+
+
+#bm25_corpus = BM25_algorithm(doc_content, k=k1, b=b)
+
+
+
+def summarize(input_query):
+
+    bm25_corpus = BM25_algorithm(doc_content, k=1.2, b=0.75)
+
+    bm25_scores = bm25_corpus.calculate_scores(input_query)
+
+    ind = np.argpartition(bm25_scores, -3)[-3:]
+    ind = ind.tolist()
+
+    relevant_passages = ' ,'.join(np.array(doc_content)[ind])
+    #bm25_context = str([i.page_content for i in relevant_passages])
+    #metadata = np.array(metadata)[ind]
+    metadata = 'placeholder'
+    similarity = np.array(bm25_scores)[ind]
+
+
+    input_text = f"given the context: {relevant_passages}, answer the question: {input_query}."
+
+    #print(bm25_context)
+
     input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
     outputs = model.generate(input_ids,
-                             do_sample=True,
-                             min_length = 50,
-                             max_length = 300,
-                             temperature = 0.7,
-                             top_p = 0.15,
-                             top_k = 50)
+                                do_sample=True,
+                                min_length = 50,
+                                max_length = 150,
+                                temperature = 0.2,
+                                top_p = 0.15,
+                                top_k = 50
+                                
+                                )
     res = tokenizer.decode(outputs[0])
 
-
-    # pipeline function Get predictions
-    '''nlp = pipeline('question-answering', model=flan_t5, tokenizer=flan_t5)
-    QA_input = {
-        'question': query,
-        'context': relevant_passages
-    }
-    res = nlp(QA_input)'''
-    #need to return the source
-
-    
-    return res, similarity, 
+        
+    return res, similarity, metadata
 
 if query:
-    all_articles = newscatcherapi.get_search(q=query,
-                                        lang='en',
-                                        countries='US',
-                                        page_size=5,
-                                        )
-    response, similarity = summarize(all_articles, query)
-    st.subheader(response)
-    st.caption(similarity)
-    #st.caption(relevant_metadata)
+    response, similarity, metadata = summarize(query)
+    st.write(response)
+    st.write('BM25 Score: ' , str(similarity))
+    #st.write(metadata)
+
 
 
         
